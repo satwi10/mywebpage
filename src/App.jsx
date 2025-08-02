06ecf9ac2ab75103e330beb929f415d3ee49f997
satwi10 <kuppilisatwik@gmail.com>
2025-08-02 21:30:58 +0530

    Setup for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  menuClick(){
+    const navlist = document.getElementById("navlist");
+    navlist.classList.toggle("active");
+  }
 
-  return (
-    <>
-     <h1>"Hey There!"</h1>
-     
-    </>
-  )
+  render() {
+    return (
+      <>
+        <header>
+          <nav>
+            <div className='logo'>My Website</div>
+            <ul id="navlist">
+              <li>Home</li>
+              <li>About</li>
+              <li>Services</li>
+              <li>Contact</li>
+            </ul>
+            <div className='menu' onClick={()=>this.menuClick()}>&#9776;</div>
+          </nav>
+        </header>
+        <section>
+          <h1>Welcome to My Responsive Website</h1>
+          <p>This design adopts that various screen sizes</p>
+        </section>
+        <footer>Copyright @ 2025. All rights reserved.</footer>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
